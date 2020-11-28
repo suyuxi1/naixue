c28934fb6df0ec126396b45e3d4f7daea2c23652
su <2296887348@qq.com>
2020-11-28 23:03:11 +0800

    地址列表显示和删除

---
+++ naixue-app/cloudfunctions-aliyun/validateToken/index.js
@@ -0,0 +1,35 @@
+'use strict';
+const jwt = require('jwt-simple')
+const db = uniCloud.database()
+exports.main = async (event, context) => {
+	const token = event.token
+	// 解码token，得到openId
+	const openId = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString())
+	const userInDB = await db.collection('users').where({
+		openId: openId
+	}).get()
+	if (userInDB.data.length !== 1) {
+		return {
+			status: -1.,
+			errCode: 'TOKEN_INVALID',
+			msg: '查无此人'
+		}
+	}
+	const userInfoDB = userInDB.data[0]
+	let userInfoDecode
+	userInfoDecode = jwt.decode(token, userInfoDB.tokenSecret)
+	if (userInDB.exp > Date.now() && userInfoDecode == userFromToken) {
+		return {
+			status: 0,
+			openId: userInfoDecode,
+			msg: 'tokeny验证成功'
+		}
+	}
+	if (userInfoDB.exp < Date.now()) {
+		return {
+			status: -3,
+			errCode: 'TOKEN_EXPIRED',
+			msg: 'token已失效'
+		}
+	}
+};
